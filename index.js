1ab1259af77c18dcf6b9970a860815770b9b1258
fariharaihan <liafariha5@gmail.com>
2022-11-26 22:21:25 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
@@ -9,6 +11,48 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpvuccm.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const bikeCategoryCollection = client.db('bikeHunt').collection('bikeCategoryCollection')
+        const bikeCollection = client.db('bikeHunt').collection('categoryCollection')
+
+
+        app.get('/bikeCategoryCollection', async (req, res) => {
+            const query = {};
+            console.log(query)
+            const options = await bikeCategoryCollection.find(query).limit(3).toArray();
+            res.send(options)
+        })
+
+        app.get('/categoryCollection', async (req, res) => {
+            const query = {};
+            console.log(query)
+            const cursor = await bikeCollection.find(query).toArray();
+            res.send(cursor)
+        })
+        app.get('/allbikes/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const query = { category_id: id }
+            const product = await bikeCollection.find(query).toArray();
+            res.send(product)
+        })
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
 app.get('/', (req, res) => {
     res.send('Bike hunt service running')
 })
@@ -16,3 +60,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Bike Hunt service running on ${port}`)
 })
+
